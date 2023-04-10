47a7ebc6b02cfbd6010ed1818cafc3f259ee627f
byulcode <bybit0408@gmail.com>
2023-04-10 18:48:45 +0900

    feat : 생성시간, 수정시간 자동생성 entity 생성

--- src/main/java/study/musical/MusicalApplication.java
+++ src/main/java/study/musical/MusicalApplication.java
@@ -2,8 +2,10 @@ package study.musical;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MusicalApplication {
 
     public static void main(String[] args) {
