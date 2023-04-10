552cc1fd63eb5457c8125b60154af6ad7722212d
byulcode <bybit0408@gmail.com>
2023-04-10 17:38:54 +0900

    initial commit

---
+++ src/main/java/study/musical/MusicalApplication.java
@@ -0,0 +1,13 @@
+package study.musical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicalApplication.class, args);
+    }
+
+}
