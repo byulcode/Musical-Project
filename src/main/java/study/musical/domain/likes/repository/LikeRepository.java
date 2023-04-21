45561be318d5de02c73b1cb43be84784fe0debc8
byulcode <bybit0408@gmail.com>
2023-04-21 17:59:31 +0900

    refactor : 좋아요 기능 오류 수정

--- src/main/java/study/musical/domain/likes/repository/LikeRepository.java
+++ src/main/java/study/musical/domain/likes/repository/LikeRepository.java
@@ -2,8 +2,14 @@ package study.musical.domain.likes.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.musical.domain.likes.entity.Likes;
+import study.musical.domain.member.entity.Member;
+import study.musical.domain.musical.entity.Musical;
 
-public interface LikeRepository extends JpaRepository<Likes, Long>, LikeCustomRepository {
+import java.util.Optional;
 
-    void deleteLikesByMemberEmailAndMusicalId(String email, Long musicalId);
+public interface LikeRepository extends JpaRepository<Likes, Long> {
+
+    void deleteLikesByMusicalAndMember(Musical musical, Member member);
+
+    Optional<Likes> findByMusicalAndMember(Musical musical, Member member);
 }
--- src/main/java/study/musical/domain/likes/service/LikeService.java
+++ src/main/java/study/musical/domain/likes/service/LikeService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import study.musical.domain.likes.dto.MusicalLikeReqDto;
 import study.musical.domain.likes.entity.Likes;
 import study.musical.domain.likes.repository.LikeRepository;
 import study.musical.domain.member.entity.Member;
@@ -27,35 +26,24 @@ public class LikeService {
     //좋아요 누르기 및 좋아요 취소
     @Transactional
     public void pushLikeButton(Long musicalId, String email) {
-        log.info("Like service pushLikeButton run..");
-        likeRepository.exist(musicalId, email)
+        Musical musical = getMusical(musicalId);
+        Member member = getMemberByEmail(email);
+
+        likeRepository.findByMusicalAndMember(musical, member)
                 .ifPresentOrElse(
-                        like -> likeRepository.deleteLikesByMemberEmailAndMusicalId(email, musicalId),
-                        () -> {
-                            Musical musical = getMusical(musicalId);
-                            Member member = memberRepository.findByEmail(email).orElseThrow();
-                            likeRepository.save(new Likes(member, musical));
-                        });
+                        like -> likeRepository.deleteLikesByMusicalAndMember(musical, member),
+                        () -> likeRepository.save(Likes.builder()
+                                .member(member)
+                                .musical(musical).build()));
     }
 
-    //사용자가 해당 뮤지컬에 좋아요를 눌렀는지 여부
-    @Transactional(readOnly = true)
-    public boolean checkPushedLike(Long musicalId, String email) {
-        log.info("Like service checkPushedLike run..");
-        return likeRepository.exist(musicalId, email)
-                .isPresent();
-    }
-
-    //뮤지컬의 좋아요 개수
-    @Transactional(readOnly = true)
-    public long getMusicalLikeCnt(MusicalLikeReqDto musicalLikeReqDto) {
-        log.info("Like service getMusicalLikeCnt run..");
-        return likeRepository.findMusicalLikeCnt(musicalLikeReqDto.getMusicalId());
-    }
-
-    @Transactional(readOnly = true)
-    public Musical getMusical(Long musicalId) {
+    private Musical getMusical(Long musicalId) {
         return musicalRepository.findById(musicalId)
                 .orElseThrow(() -> new MusicalNotExistException(ErrorCode.MUSICAL_NOT_EXIST));
     }
+
+    private Member getMemberByEmail(String email) {
+        return memberRepository.findByEmail(email).orElseThrow();
+    }
+
 }
